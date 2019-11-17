60975039d64ed034023dd68dd921b7abea8deb60
roland <43933368+roland-zauner@users.noreply.github.com>
2019-11-17 19:09:20 +0100

    init version

---
+++ install.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
+install.packages("factoextra")
+install.packages('BiocManager')
+BiocManager::install('EnhancedVolcano')
+
