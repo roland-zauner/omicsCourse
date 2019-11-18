f200f144d5116fe74a2b3ad29a77c2bdf98d4161
roland <43933368+roland-zauner@users.noreply.github.com>
2019-11-18 14:40:22 +0100

    add tidyverse and other general libraries

--- install.R
+++ install.R
@@ -1,3 +1,8 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages('leaflet')
 install.packages("openxlsx", dependencies = TRUE)
 install.packages("ggpubr")
 install.packages("factoextra")
